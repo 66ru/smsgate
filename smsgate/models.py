4d5cbc7ca86ff33b45d86f95e44d93259f1c94b1
Nikita Hismatov <hismatovns@gmail.com>
2011-06-21 14:30:19 +0600

    партнер - в модель

--- smsgate/models.py
+++ smsgate/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 
+class Partner(models.Model):
+    name = models.CharField(max_length=20)
+
+
 class QueueItem(models.Model):
     phone_n = models.CharField(max_length=15)
+
+    partner = models.ForeignKey(Partner)
     
     created = models.DateTimeField(auto_now_add=True)
     changed = models.DateTimeField(auto_now=True)
