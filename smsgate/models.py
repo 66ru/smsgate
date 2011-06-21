14ddc923f31d3dc046d68e49d7a85c4e36d6dfc9
Nikita Hismatov <hismatovns@gmail.com>
2011-06-21 18:41:05 +0600

    добавил comment в модель

--- smsgate/models.py
+++ smsgate/models.py
@@ -7,10 +7,9 @@ class Partner(models.Model):
 
 class QueueItem(models.Model):
     phone_n = models.CharField(max_length=15)
-
     message = models.CharField(max_length=140)
-
     partner = models.ForeignKey(Partner)
+    comment = models.TextField(blank=True)
     
     created = models.DateTimeField(auto_now_add=True)
     changed = models.DateTimeField(auto_now=True)
