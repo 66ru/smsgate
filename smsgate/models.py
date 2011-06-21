a901f46c8512acc9e0f427fdff181e9326eda66d
Nikita Hismatov <hismatovns@gmail.com>
2011-06-21 14:34:03 +0600

    message в модели

--- smsgate/models.py
+++ smsgate/models.py
@@ -8,6 +8,8 @@ class Partner(models.Model):
 class QueueItem(models.Model):
     phone_n = models.CharField(max_length=15)
 
+    message = models.CharField(max_length=140)
+
     partner = models.ForeignKey(Partner)
     
     created = models.DateTimeField(auto_now_add=True)
