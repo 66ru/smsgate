a5d72b4930d2d9e1c54e743fc04204556042eb3b
Nikita Hismatov <hismatovns@gmail.com>
2011-06-21 14:21:58 +0600

    модель

--- smsgate/models.py
+++ smsgate/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class QueueItem(models.Model):
+    phone_n = models.CharField(max_length=15)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
+
+    STATUS_CHOICES = (
+        ('0', 'IN PROGRESS'),
+        ('1', 'OK'),
+        ('2', 'FAILED'),
+    )
+    status = models.CharField(max_length=1)
+    status_message = models.TextField(blank=True)
