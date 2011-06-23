11eb3ac22ab40cec64c7b4a43e593b1a8fe93aaf
Nikita Hismatov <hismatovns@gmail.com>
2011-06-23 15:06:50 +0600

    более понятная запись process_request

--- smsgate/auth/middlware.py
+++ smsgate/auth/middlware.py
@@ -3,11 +3,10 @@ from django.contrib.auth import authenticate
 
 class PartnerPostTokenMiddleware(object):
     def process_request(self, request):
-        if request.method != 'POST' or \
-            'id' not in request.POST or \
-            'token' not in request.POST:
+        if request.method != 'POST' or 'id' not in request.POST or 'token' not in request.POST:
             return
-        id = int(request.POST['id'])
-        token = request.POST['token']
-        user = authenticate(id=id, token=token)
-        request.user = user
+        else:
+            id = int(request.POST['id'])
+            token = request.POST['token']
+            user = authenticate(id=id, token=token)
+            request.user = user
