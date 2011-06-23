00d3e172b288885dbd3fc624ffdb8dc1b49243fa
Nikita Hismatov <hismatovns@gmail.com>
2011-06-23 16:44:50 +0600

    функции для работы с ip: проверка, входит ли ip в диапазон

--- smsgate/auth/__init__.py
+++ smsgate/auth/__init__.py
@@ -27,3 +27,32 @@ def permission_required_or_403(perm):
     enabled, redirecting to the log-in page or rendering a 403 as necessary.
     """
     return user_passes_test_or_403(lambda u: u.has_perm(perm))
+
+
+def _ipv4_to_int(ip):
+    """
+    >>> f = _ipv4_to_int
+    >>> f('0.0.0.0')
+    0L
+    >>> f('255.255.255.255')
+    4294967295L
+    """
+    hexn = ''.join(["%02X" % long(i) for i in ip.split('.')])
+    return long(hexn, 16)
+
+
+def ip_in_range(input_ip, ip_range_from, ip_range_to='255.255.255.255'):
+    """
+    >>> f = ip_in_range
+    >>> f('127.0.0.1', '0.0.0.0', '255.255.255.255')
+    True
+    >>> f('127.0.0.1', '127.0.0.0', '255.255.255.255')
+    True
+    >>> f('127.0.0.1', '100.1.1.1', '255.255.255.255')
+    True
+    >>> f('127.0.0.1', '127.0.0.1')
+    True
+    >>> f('127.0.0.1', '127.0.0.0')
+    True
+    """
+    return _ipv4_to_int(ip_range_from) <= _ipv4_to_int(input_ip) <= _ipv4_to_int(ip_range_to)
